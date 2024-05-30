26eda91d365ff6c51aff0d96e38d290cfd09ca1b
Rafael <raphaelmenezesvill@gmail.com>
2024-05-30 14:21:46 -0300

    The frontend page has been create (culd be better but i dont like frontend), next step is to creat the frontend logic to load the content in page

--- Frontend/scripts/script.js
+++ Frontend/scripts/script.js
@@ -1,11 +1,14 @@
-const url = 'http://127.0.0.1:5000/iframes'
-const lista_de_urls = ["https://www.youtube.com/@rbiana/videos","https://www.youtube.com/@Mamaefalei/videos"]
+var iframes = ""
 
-$.ajax({
-    type: 'POST',
-    url: url,
-    data: JSON.stringify({channels: lista_de_urls, videos_quantity_peer_channel: 2}),
-    contentType: "application/json",
-    success: function(data){console.log(data)},
-    dataType:'json'
-})
+function load_iframes(){    
+    const url = 'http://127.0.0.1:5000/iframes'
+    const lista_de_urls = ["https://www.youtube.com/@rbiana/videos","https://www.youtube.com/@Mamaefalei/videos"]
+    $.ajax({
+        type: 'POST',
+        url: url,
+        data: JSON.stringify({channels: lista_de_urls, videos_quantity_peer_channel: 2}),
+        contentType: "application/json",
+        success: function(data){console.log(data); iframes = data },
+        dataType:'json'
+    })
+}
