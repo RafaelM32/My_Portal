20918bef2e8bd1af87a369a510d066de4b0f704e
Rafael <raphaelmenezesvill@gmail.com>
2024-06-06 07:06:19 -0300

    Agora autorizei apenas a origem vinda do frontend

--- Backend/main.py
+++ Backend/main.py
@@ -20,7 +20,7 @@ def aplication_status():
 def iframes():
 
     requisicao = request.json
-    resposta = {'ok': True, 'iframes_list': [], 'cabecalho': str(request.headers)}
+    resposta = {'ok': True, 'iframes_list': []}
 
     
     if not is_more_the_x_minutes(10) and not requisicao['refresh']: #If did not pass 10 minutes and you dont use the refresh button 
